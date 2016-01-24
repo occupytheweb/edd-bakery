c89a58aed5d11ff65a1da12f992f96af70912e11
occupytheweb <afinegameofnil@gmail.com>
2016-01-25 02:35:22 +0400

    implemented fieldset resolving

--- dbinterface.php
+++ dbinterface.php
@@ -1,50 +1,72 @@
 <?php
 namespace dbinterface {
 
-Class PDO_h extends \PDO {
-		private static $DB = [
-			"name" => "edd",
-			"user" => "root",
-			"pass" => "superuser",
-		];
+# Here be dragons.
+# Thou art forewarned.
 
-		private static $data_sources = [
-			"users" => "edd.users_tbl",
-		];
+  class PDO_handle extends \PDO
+  {
+      private static $DB = [
+        "name" => "edd",
+        "user" => "root",
+        "pass" => "superuser",
+      ];
 
-		private $DSN = 'mysql:host=127.0.0.1;dbname =';
-		private $data_route;
+      private static $data_sources = [
+        "users" => "edd.users_tbl",
+      ];
 
-		public static function exception_handler($exception) {
-			  /*just*/ die('Unhandled exception: ' . $exception->$getMessage());
-		}
+      private static $data_source_fieldset = [
+        "users" => array("UserID", "FirstName", "LastName", "Username", "Password"),
+      ];
 
-		public function __construct() {
-				$this->DSN .= static::$DB['name'];
+      private $DSN = 'mysql:host=127.0.0.1;dbname =';
+      private $data_route;
 
-				set_exception_handler(array(__CLASS__, 'exception_handler'));
+      public static function exception_handler($exception) {
+          /*just*/ die('Unhandled exception: ' . $exception->$getMessage());
+      }
 
-			  parent::__construct($this->DSN, static::$DB['user'], static::$DB['pass']);
-			  
-			  $this->data_route = static::$data_sources['users'];
-			  $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-				$this->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
-		
-		    restore_exception_handler();
-		}
+      public function __construct() {
+          $this->DSN .= static::$DB['name'];
 
-		public function pull($arg, &$result) {
-				$stmt = $this->prepare('SELECT :arg FROM ' . $this->data_route);
-				$stmt->bindParam(':arg', $arg);
-				$stmt->execute();
-				$record = $stmt->fetch();
-				return $record;
-		}
+          set_exception_handler(array(__CLASS__, 'exception_handler'));
 
-}
+          parent::__construct($this->DSN, static::$DB['user'], static::$DB['pass']);
+
+          $this->data_route = static::$data_sources['users'];
+          $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+          $this->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+
+          restore_exception_handler();
+      }
+
+      public function pull($arg, &$result, $fieldset = "*") {
+          $fieldset = self::resolve_field($fieldset);
+          $stmt = $this->prepare('SELECT ' . $fieldset . ' FROM ' . $this->data_route);
+          //$stmt->bindParam(':arg', $arg);
+          $stmt->execute();
+          $record = $stmt->fetch();
+          return $record;
+      }
+
+      private function resolve_field($indexes) {
+          if(!($indexes == "*")) {
+              $fields = "";
+              foreach( explode(" ", $indexes)  as $thisIndex ) {
+                  $fields .= static::$data_source_fieldset[ array_search($this->data_route, static::$data_sources) ][ $thisIndex ] . ",";
+              }
+              $fields = trim($fields, ",");
+          } else {
+              $fields = "*";
+          }
+
+          return $fields;
+      }
+
+  }
 }
 
 namespace {
-	$dbh = new dbinterface\PDO_h();
+  $dbh = new dbinterface\PDO_handle();
 }
-?>
