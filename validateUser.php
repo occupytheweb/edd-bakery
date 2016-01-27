1935a2f60145ffb6deeaa7bfaf9f821f12d1d130
occupytheweb <afinegameofnil@gmail.com>
2016-01-27 20:03:15 +0400

    restricted pulled columns | string cast to notify failure

--- validateUser.php
+++ validateUser.php
@@ -5,12 +5,12 @@ header('Cache-Control: no-cache, must-revalidate');
 require_once('dbinterface.php');
 
 $unsafe_un = isset( $_POST['u_n'] ) ? $_POST['u_n'] : false;
-$exists = false;
+$exists = 'false';
 
 if($unsafe_un) {
   $db_handle = new dbinterface\PDO_handle();
   
-  $record = ($db_handle->pull("3", ['Username' => "$unsafe_un"], $exists))[0];
+  $record = $db_handle->pull("1 2 3", ['Username' => "$unsafe_un"], $exists)[0];
   $res = [
     'fname' => $record['FirstName'],
     'lname' => $record['LastName'],
