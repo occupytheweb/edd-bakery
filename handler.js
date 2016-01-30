92167fabbddfb82dc28094a435f6a42658543ab5
occupytheweb <afinegameofnil@gmail.com>
2016-01-30 21:45:40 +0400

    refactored event listener - 'this' reference bound to event initiator

--- handler.js
+++ handler.js
@@ -1,22 +1,21 @@
 //mess ahead
 //ui_namespace = ui_namespace || {};
-var drops = document.getElementsByClassName("dropdown");
 
-function listen(itm) {
-  //console.log(itm);
-  console.log(itm.className + " idx: " + itm.className.indexOf(" open"));
-  itm.className.indexOf(" open") === -1 ? itm.className += " open" : itm.className = itm.className.replace(" open", "");
-}
+var click_handler = function() {
+    function listen(itm) {
+        itm.className.indexOf(" open") === -1 ? itm.className += " open" : itm.className = itm.className.replace(" open", "");
+    };
+
+    var drops = document.getElementsByClassName("dropdown");
+    var i = drops.length;
+    while(i--) {
+        d = drops[i];
+        d.addEventListener("click", function(){ listen(this); }, false);
+    }
+};
+
+
 
 window.onload = function() {
-//    var drops = document.getElementsByClassName("dropdown");
-    var i = drops.length;
-    console.log("length: " + i);
-    while(i--) {
-      console.log("i1: " + i);
-      d = drops[i];
-      //console.log(d);
-      d.addEventListener("click", function(){ listen(d); }, false);
-      //d.bind("click", function() { listen(d); }, false );
-    }
+    click_handler();
 };
