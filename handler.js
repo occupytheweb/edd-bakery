10578f73fbbbd2d69fb5fc91ea7f176083ec620a
occupytheweb <afinegameofnil@gmail.com>
2016-02-07 18:09:55 +0400

    keep child elements from responding to parents event bubbles

--- handler.js
+++ handler.js
@@ -2,15 +2,17 @@
 //ui_namespace = ui_namespace || {};
 
 var click_handler = function() {
-    function listen(itm) {
-        itm.className.indexOf(" open") === -1 ? itm.className += " open" : itm.className = itm.className.replace(" open", "");
+    function listen(itm, e) {
+        if(e.target == "") {
+            itm.className.indexOf(" open") === -1 ? itm.className += " open" : itm.className = itm.className.replace(" open", "");
+        }
     };
 
     var drops = document.getElementsByClassName("dropdown");
     var i = drops.length;
     while(i--) {
         d = drops[i];
-        d.addEventListener("click", function(){ listen(this); }, false);
+        d.addEventListener("click", function(){ listen(this, event); }, false);
     }
 };
 
