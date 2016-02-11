525d65b86e87dc9cab504717a22d120f949429d3
occupytheweb <afinegameofnil@gmail.com>
2016-02-12 02:50:50 +0400

    Table structure : store_cakes

--- edd.sql
+++ edd.sql
@@ -24,7 +24,6 @@ USE `edd`;
 --
 -- Table structure for table `users`
 --
-
 DROP TABLE IF EXISTS `users_tbl`;
 CREATE TABLE `users_tbl` (
   `UserID` int(255) unsigned NOT NULL,
@@ -34,17 +33,34 @@ CREATE TABLE `users_tbl` (
   `Password` varchar(25) CHARACTER SET latin1 NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_520_ci;
 
+--
+-- Table structure for table `store_cakes`
+--
+DROP TABLE IF EXISTS `store_cakes_tbl`;
+CREATE TABLE `store_cakes_tbl` (
+  `ID` int(10) unsigned NOT NULL,
+  `Name` varchar(75) COLLATE utf16_unicode_520_ci NOT NULL,
+  `Price` int(10) NOT NULL,
+  `ImageBasePath` varchar(75) COLLATE utf16_unicode_520_ci NOT NULL,
+  `Details` text COLLATE utf16_unicode_520_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_520_ci;
+
+
+-- Test Data
 
 --
--- `users` table Data data for table `users`
+-- `users` data dump
 --
-
 INSERT INTO `users_tbl` (`UserID`, `FirstName`, `LastName`, `Username`, `Password`) VALUES
 (1, 'admin', '-', 'eddadmin', 'root@eddadmin_0x');
 
 --
--- Indexes for dumped tables
+-- `store_cakes` data dump
 --
+INSERT INTO `store_cakes_tbl` (`ID`, `Name`, `Price`, `ImageBasePath`, `Details`) VALUES
+(1, 'Baker''s Choice Bars Assortment', 300, 'bars_assortment', 'A beautiful and delicious assortment of our bakery’s double fudge brownies, chocolate chunk blondies and magic cookie bars.'),
+(2, 'Chocolate Lover''s Cookie Assortment', 450, 'choc-cookies_assortment', 'Enjoy each of our six extra-large cranberry white-chocolate chunk cookies made with tart dried cranberries and toasted pecans, chocolate dipped coconut flavored macaroons with crisp exterior and a soft, chewy interior and a dozen chocolate chunk cookies.');
+
 
 --
 -- Indexes for table `users`
@@ -53,15 +69,28 @@ ALTER TABLE `users_tbl`
   ADD PRIMARY KEY (`UserID`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `store-cakes`
 --
+ALTER TABLE `store_cakes_tbl`
+  ADD PRIMARY KEY (`ID`);
+
+
+-- AUTO_INCREMENT for dumped tables
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users_tbl`
   MODIFY `UserID` int(255) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
-  
+
+--
+-- AUTO_INCREMENT for table `store-cakes`
+--
+ALTER TABLE `store_cakes_tbl`
+  MODIFY `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+
+
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
