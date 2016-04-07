8941adcd952d7339dd770776a81d670320dcd37c
neshen <neshboss_05@hotmail.com>
2016-04-07 15:04:12 +0400

    Gallery
    -cakes section
    -pastries section

---
+++ cakes.php
@@ -0,0 +1,114 @@
+<!DOCTYPE>
+<html>
+
+<head>
+<title>Gallery</title>
+  <link rel="stylesheet" href="css/display.css" />
+  <link rel="stylesheet" href="css/typography.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+  <link rel="stylesheet" href="css/grids.css" />
+  <link rel="stylesheet" href="css/glyphs.css" />
+  <link rel="stylesheet" href="css/dropdown.css" />
+  <link rel="stylesheet" href="css/store.css" />
+  <link rel="stylesheet" href="css/gallery.css" />
+</head>
+
+<body background="bakery-background-1.jpg">
+
+  <?php
+    require_once('header.php');
+ ?> 
+  <h2 style="text-align:center">Pastries</h2>
+ 
+ <div class="img">
+  <a target="_blank" href="res/cakes/c2.jpg">
+    <img src="cakes/c2.jpg" alt="breads" width="300" height="200">
+  </a>
+</div>
+
+<div class="img">
+  <a target="_blank" href="res/cakes/c1.jpg">
+    <img src="cakes/c1.jpg" alt="breads" width="300" height="200">
+  </a>
+</div>
+
+<div class="img">
+  <a target="_blank" href="res/cakes/c3.jpg">
+    <img src="cakes/c3.jpg" alt="breads" width="300" height="200">
+  </a>
+</div>
+
+<div class="img">
+  <a target="_blank" href="res/cakes/c4.jpg">
+    <img src="cakes/c4.jpg" alt="breads" width="300" height="200">
+  </a>
+</div>
+
+<div class="img">
+  <a target="_blank" href="res/cakes/c5.jpg">
+    <img src="cakes/c5.jpg" alt="breads" width="300" height="200">
+  </a>
+</div>
+
+<div class="img">
+  <a target="_blank" href="res/cakes/c6.jpg">
+    <img src="cakes/c6.jpg" alt="breads" width="300" height="200">
+  </a>
+</div>
+
+<div class="img">
+  <a target="_blank" href="res/cakes/c7.jpg">
+    <img src="cakes/c7.jpg" alt="breads" width="300" height="200">
+  </a>
+</div>
+
+<div class="img">
+  <a target="_blank" href="res/cakes/c8.jpg">
+    <img src="cakes/c8.jpg" alt="breads" width="300" height="200">
+  </a>
+</div>
+
+<div class="img">
+  <a target="_blank" href="res/cakes/c9.jpg">
+    <img src="cakes/c9.jpg" alt="breads" width="300" height="200">
+  </a>
+</div>
+
+<div class="img">
+  <a target="_blank" href="cakes/c10.jpg">
+    <img src="cakes/c10.jpg" alt="breads" width="300" height="200">
+  </a>
+</div>
+
+<footer>
+					<div class="col-sm-12">
+				  	<div class="col-md-offset-2 col-md-3">
+
+						</div>
+
+						<div class="col-md-offset-0 col-md-3">
+
+						</div>
+
+
+
+						<div class="col-md-offset-0 col-md-3">
+							<div class="vcard">
+								<h4 class="coral"><a href="contact/">Contact <span class="fn org">Epi de Dieu Ltd</span></a></h4>
+								<p class="tel">465 1001</p>
+
+								<div class="adr">
+									<p>
+										<span class="street-address">Rémi Ollier Street</span><br />
+										<span class="locality">Quatre-Bornes</span>, <span class="region">PW</span>, <span class="postal-code">0000</span>
+									</p>
+								</div>
+
+								<p><span class="tel"><span class="type">F<span class="d">ax</span></span>: <span class="value">(+230) 465 1001</span></span></p>
+								<p class="e">E<span class="d">mail</span>: <a href="&#99;&#111;&#110;&#116;&#97;&#99;&#116;&#64; &#108;&#111;&#99;&#97;&#108;&#104;&#111;&#115;&#116;" class="email that-thing"><span class="that-thing" style="unicode-bidi: bidi-override; direction: rtl;"> &#116;&#115;&#111;&#104;&#108;&#97;&#99;&#111;&#108;&#64;&#116;&#99;&#97;&#116;&#110;&#111;&#99; </span></a></p>
+							</div><!-- /.vcard -->
+						</div>
+					</div>
+					<div style="clear: both; height: 25px; width: 100%; bottom: 0px; background-image: url(res/ft.png);">
+					</div>
+				</footer>
