6cd38250835a379fd1467b6557b3b6ed0cacd439
neshen <neshboss_05@hotmail.com>
2016-04-07 14:39:17 +0400

    About Us :: markup

---
+++ about_us.php
@@ -0,0 +1,74 @@
+<!DOCTYPE>
+<html>
+
+<head>
+<title>About Us</title>
+  <link rel="stylesheet" href="css/display.css" />
+  <link rel="stylesheet" href="css/typography.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+  <link rel="stylesheet" href="css/grids.css" />
+  <link rel="stylesheet" href="css/glyphs.css" />
+  <link rel="stylesheet" href="css/dropdown.css" />
+  <link rel="stylesheet" href="css/store.css" />
+</head>
+
+<body background="background.jpg">
+
+  <?php
+    require_once('header.php');
+ ?> 
+<div style=" height: 100%;float: left;display: inline-block">	
+<h1><strong><u>Welcome to Epi De Dieu</strong></u></h1>
+
+
+<h3 class="">Our Story</h3>
+
+
+<p><font size="3">Epi de Dieu is a noteworthy bakery and pastry shop in Quatres-Bornes.
+It has been set up in 2002 with the intent of providing the vicinity with baked goods and all things cake. At start, the two owners, Mr and Mrs Thomas could run
+the business smoothly by themselves. After one year, the number of client climbed up significantly and the owners hired three qualified workers from
+Bangladesh to bake the cakes and breads. Actually, there are eight workers  in total working at Epi de Dieu, the owners inclusive.
+
+As part of its provided services, the bakery produces a certain daily supply of bread and cakes intended for individual consumption as well as for supplying local shops. 
+The shop also provides catering services for special occasions such as parties, weddings or anniversaries.
+We take proud in what we do and it shows. Only flavor exceeds the beauty of Epi De Dieu's exquisite  breads,cakes, wedding cakes and pastries. Our main objective is to 
+deliver value and an excellent customer service. 
+
+</p></font>
+
+</br>
+</br>
+<img src="image5" style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.5em;" />
+<img src="image1" style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.5em;" />
+<img src="image2" style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.5em;" />
+<img src="image3" style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.5em;" />
+<img src="image4" style="float: left; width: 30%; margin-right: 1%; margin-bottom: 0.5em;" />
+<p style="clear: both;">
+<br/>
+<br/>
+</div>
+
+<div  style="width: 12.5%;height: 100%;float: left;display: block;position: relative">
+  <h3>Need a cake?</h3>
+  <p>Call today</p>
+ </div>
+ 
+<div class=""><strong>Company Information</strong></div></br>
+<div class="">
+Email:<a href="epi@gmail.com">epi@gmail.com</a></br>
+Contact Number: 4272565 | 57985211 </br>
+Address: 5,Avenue Murphy,Quatres-Bornes</br>
+</div>
+
+</br></br></br>
+
+<footer style="text-align:center">
+DAILY SELECTION OF BREADS, PASTRIES, COOKIES, CAKES AND MORE AVAILABLE FOR PICKUP ANY TIME!
+MONDAY-FRIDAY 8 A.M.-6 P.M. | SATURDAY 10 A.M.-4 P.M. </br>
+Epi De Dieu
+</footer>
+
+
+
+</body>
+</html>
