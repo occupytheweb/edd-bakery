b44e3d1a334a6994fbb9786136351538e6dae447
yeshvin <yeshvin@live.com>
2016-04-09 00:40:12 +0400

    updated forms
    without css

--- Forms/businessform.php
+++ Forms/businessform.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
-	<script type="text/javascript" src="form.js"></script>
+		<script type="text/javascript" src="form.js"></script>
 		<title>EdD Bakery and Pastry Shop</title>
 		<link rel="stylesheet" href="css/customizeC.css"/>
 
@@ -48,12 +48,47 @@
 						<option value="">Pick a Frosting</option>
 						<option value="no_frost">No Frosting</option>
 						<option value="choco_buttercream">Chocolate Buttercream</option>
+						<option value="mocca_buttercream">Mocca Buttercream</option>
+						<option value="lemon_buttercream">Lemon Buttercream</option>
+						<option value="vanilla">Whipped Vanilla Buttercream Frosting</option>
+						<option value="orange">White Chocolate Whipped cream</option>
 						<option value="white_chocolate">White Chocolate Whipped cream</option>
-					</select>
+						<option value="strawberry">Strawberry Buttercream</option>
+						<option value="raspberry">Raspberry Buttercream</option>
+						<option value="peach">Peach Buttercream</option>
+						<option value="strawberry">Strawberry Buttercream</option>
+						<option value="caramel">Caramel Buttercream</option>
+					</select><br></br>
 					<span id="frostingIm"></span>
+					<label for="fillings">Filling Type:<em>*</em></label>
+					<select id="filling" onchange="selectedFilling()">
+						<option value="">Select a Filling</option>
+						<option value="no_filling">No Filling</option>
+						<option value="coconut">Coconut Pecan</option>
+						<option value="lemoncurd">Lemon Curd</option>
+						<option value="oreo">Oreo Mousse</option>
+						<option value="pastrycream">Pastry Cream</option>
+						<option value="peachjam">Peach Jam</option>
+						<option value="peachmousse">Peach Mousse</option>
+						<option value="raspberryjam">Raspberry Jam</option>
+						<option value="strawberrymousse">Strawberry Mousse</option>
+						<option value="whitechoco">White Chocoloate Mousse</option>
+					</select><br></br>
+					<span id="fillingIm"></span>
 					<h2>Special Instructions:</h2>
 					<label for="theme_cake">Theme:</label>
 					<input type="text" name="theme_cake" id="theme_cake"/> <br><br />
+					<label for="inscription">Inscription:</label>
+					<input type="text" name="inscription" id="inscription"/> <br><br />
+					<label for="colour">Does the cake require any specific colour?</label>
+					<textarea name="colour" id="abc" rows="5" cols="40"/></textarea><br></br>
+					<label for="allergy">Any Allergies?</label>
+					<textarea name="allergy" id="allergy" rows="5" cols="40"></textarea><br></br>
+					<label for="colour">Does the cake require any specific colour?:</label>
+					<textarea name="colour" id="abc" rows="5" cols="40"/></textarea><br></br>
+					<label for="Add_notes">Additional Notes:</label>
+					<textarea name="Add_notes" id="Add_notes" rows="5" cols="40"/></textarea><br></br>
+					<input type="button" id="buttn" value="Place order"/>
 				</form>
 
 			</div>
--- Forms/form.js
+++ Forms/form.js
@@ -3,18 +3,91 @@
 function selectedFrosting() {
 		var frostType     = document.getElementById('frosting').value;
 		var frostSelected = document.getElementById('frostingIm');
-		var msg           = "";
+		var imgs          = "";
 
 		switch(frostType) {
 				case "no_frost":
-						msg = "No Frosting";
+						imgs = "No Frosting";
 						break;
 				case "choco_buttercream":
-						msg = "Chocolate Buttercream";
+						imgs = "<img src='Frosting_type/chocolate_buttercream.jpg'></img>";
+						break;
+				case "mocca_buttercream":
+						imgs = "<img src='Frosting_type/mocca.jpg'></img>";
+						break;
+				case "lemon_buttercream":
+						imgs = "<img src='Frosting_type/lemon.jpg'></img>";
+						break;
+				case "vanilla":
+						imgs = "<img src='Frosting_type/Whipped_Vanilla_Buttercream_Frosting.jpg'></img>";
+						break;
+				case "orange":
+						imgs = "<img src='Frosting_type/orange.jpg'></img>";
+						break;
+				case "white_chocolate":
+						imgs = "<img src='Frosting_type/white_chocolate_whipped_cream.jpg'></img>";
+						break;
+				case "strawberry":
+						imgs= "<img src='Frosting_type/Strawberry.jpg'></img>";
+						break;
+				case "raspberry":
+						imgs= "<img src='Frosting_type/raspberry.jpg'></img>";
+						break;
+				case "peach":
+						imgs= "<img src='Frosting_type/peach.jpg'></img>";
+						break;
+	  		case "strawberry":
+						imgs= "<img src='Frosting_type/Strawberry.jpg'></img>";
+						break;
+				case "caramel":
+						imgs= "<img src='Frosting_type/caramel.jpg'></img>";
 						break;
 				default:
-						msg = "Please choose a frosting";
+						imgs = "Please choose a frosting";
 		}
 
-		frostSelected.innerHTML = msg;
+		frostSelected.innerHTML = imgs;
+}
+
+function selectedFilling() {
+	var fillingType				= document.getElementById('filling').value;
+	var fillingSelected		=	document.getElementById('fillingIm');
+	var	img							="";
+
+	switch(fillingType){
+
+		case "No_filling":
+			imgs= "No filling";
+			break;
+		case "coconut":
+			img= "<img src='Filling/coconutpecan.jpg'></img>";
+			break;
+		case "oreo":
+			img= "<img src='Filling/oreomousse.jpg'></img>";
+			break;
+		case "lemoncurd":
+			img= "<img src='Filling/lemoncurd.jpg'></img>";
+			break;
+		case "pastrycream":
+			img= "<img src='Filling/pastrycream.jpg'></img>";
+			break;
+		case "peachjam":
+			img= "<img src='Filling/peachejam.jpg'></img>";
+			break;
+		case "peachmousse":
+			img= "<img src='Filling/peachmousse.jpg'></img>";
+			break;
+		case "raspberryjam":
+			img= "<img src='Filling/Raspberryjam.jpg'></img>";
+			break;
+		case "strawberrymousse":
+			img= "<img src='Filling/strawberrymousse.jpg'></img>";
+			break;
+		case "whitechoco":
+			img= "<img src='Filling/white.jpg'></img>";
+			break;
+		default:
+			img= "Please choose a Filling";
+	}
+	fillingSelected.innerHTML	=	img;
 }
