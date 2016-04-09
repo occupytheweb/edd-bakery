be557c1c3b59a74e6997f912161b45e8ad299ce2
occupytheweb <afinegameofnil@gmail.com>
2016-04-09 22:00:20 +0400

    Header section :: Reflect user login status

--- header.php
+++ header.php
@@ -1,93 +1,119 @@
 <?php
-?>
-  <header class="navbar" role="navigation">
-    <div id="bg">
-    </div>
 
-    <div class="container">
-      <div class="navbar-header">
+require_once('persistence.class.php');
 
-        <a id="nav-logo" href="/">
-          <!-- edd logo -->
-        </a>
-        </div><!-- ./navbar-header -->
+$right_drop_type = persistence::user()->authenticated() ? "profile" : "login";
+$username        = persistence::user()->username;
 
-      <div id="contact">
 
+$login = <<<LOGIN
+<li class="nav-login dropdown" id="{$right_drop_type}">
+  <a>Login</a>
+
+  <ul class="dropdown-menu">
+    <li>
+      <span>
+        <form action="login.php" method="POST" id="nav-form">
+         <input type="email" placeholder="e-mail" name="alias"/>
+        </form>
+      </span>
+    </li>
+    <li id="pass">
+      <input type="password" placeholder="password" name="password"/>
+      <button type="button" id="register_btn">Register</button>
+      <button type="button" id="login_btn">Login</button>
+    </li>
+  </ul>
+</li>
+LOGIN;
+
+$profile = <<<PROFILE
+<li class="nav-login dropdown" id="{$right_drop_type}">
+  <a>{$username}</a>
+
+  <ul class="dropdown-menu">
+    <li><a>Profile</a></li>
+    <li><a>Sign Out</a></li>
+  </ul>
+</li>
+PROFILE;
+
+
+$header = <<<HEAD
+<header class="navbar" role="navigation">
+  <div id="bg">
+  </div>
+
+  <div class="container">
+    <div class="navbar-header">
+
+      <a id="nav-logo" href="/">
+        <!-- edd logo -->
+      </a>
       </div>
 
-      <nav class="nav-menu">
-        <ul class="nav navbar-nav">
-          <li>
-            <a href="">Home</a>
-          </li>
+    <div id="contact">
 
-          <li class="dropdown">
-            <a class="dropdown-toggle">
-              Cakes
-              <span class="glyphicon glyphicon-menu-down"></span>
-            </a>
-            <!--TODO:: glyph here-->
-            <ul class="dropdown-menu">
-              <li>
-                <a href="#">Wedding Cakes</a>
-              </li>
-              <li class="divider"></li>
-              <li>
-                <a href="#">Birthday Cakes</a>
-              </li>
-              <li class="divider"></li>
-              <li>
-                <a href="#">Cupcakes</a>
-              </li>
-              <li class="divider"></li>
-              <li>
-                <a href="#">Treats & Flavours</a>
-              </li>
-            </ul>
-          </li>
+    </div>
 
-          <li>
-            <a href="#">Bread</a>
-          </li>
+    <nav id="sticky-menu" class="nav-menu">
+      <ul class="nav navbar-nav">
+        <li>
+          <a class="tab" href="">Home</a>
+        </li>
 
-          <li>
-            <a href="#">Store</a>
-          </li>
+        <li class="dropdown">
+          <a class="dropdown-toggle tab">
+            Cakes
+            <span class="glyphicon glyphicon-menu-down"></span>
+          </a>
+          <!--TODO:: glyph here-->
+          <ul class="dropdown-menu">
+            <li>
+              <a href="#">Wedding Cakes</a>
+            </li>
+            <li class="divider"></li>
+            <li>
+              <a href="#">Birthday Cakes</a>
+            </li>
+            <li class="divider"></li>
+            <li>
+              <a href="#">Cupcakes</a>
+            </li>
+            <li class="divider"></li>
+            <li>
+              <a href="#">Treats &amp; Flavours</a>
+            </li>
+          </ul>
+        </li>
 
-          <li>
-            <a href="#">About</a>
-          </li>
+        <li>
+          <a class="tab">Bread</a>
+        </li>
 
-          <li>
-            <a href="#">FAQ</a>
-          </li>
+        <li>
+          <a class="tab">Store</a>
+        </li>
 
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
+        <li>
+          <a class="tab">About</a>
+        </li>
 
-        <ul class="nav navbar-nav navbar-right">
-          <li class="nav-login dropdown" id="login">
-            <a>Login</a>
+        <li>
+          <a class="tab">FAQ</a>
+        </li>
 
-            <ul class="dropdown-menu">
-              <li>
-              <span class="invisble">
-                <form action="login.php" method="POST" id="nav-form">
-                 <input type="email" placeholder="e-mail" name="alias"/>
-                </form>
-              </span>
-              </li>
-              <li id="pass">
-                <input type="password" placeholder="password" name="password"/>
-                <button type="button">Register</button>
-                <button type="button">Login</button>
-              </li>
-            </ul><!-- /.dropdown-menu -->
-          </li><!-- /.nav-login -->
-        </ul><!-- /.nav navbar-nav navbar-right dropdown -->
-      </nav><!-- /.nav-menu -->
-    </div><!-- /.container -->
-  </header><!-- /.navbar -->
+        <li>
+          <a class="tab">Contact</a>
+        </li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+        {${$right_drop_type}}
+      </ul>
+    </nav>
+  </div>
+</header>
+HEAD;
+
+echo $header;
