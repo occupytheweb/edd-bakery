24f862a34229e6c4a9ac7fec6006a17214920bd1
occupytheweb <afinegameofnil@gmail.com>
2016-04-09 21:11:49 +0400

    Product :: Class definition extension + HTML-safe output
    - Markup & details data sent from the object is HTML-encoded
    - Deprecates internal json-encoding of Product details

--- Product.class.php
+++ Product.class.php
@@ -6,6 +6,7 @@ class Product
 {
     private $type;
     private $id;
+    private $sharedID;
     private $name;
     private $details;
     private $price;
@@ -13,26 +14,15 @@ class Product
     private $active;
 
 
-    private function data_encode() {
-        $data = [
-                  'id'      => $this->id,
-                  'name'    => $this->name,
-                  'details' => $this->details,
-                  'price'   => $this->price
-                ];
-
-        return json_encode($data);
-    }
-
-
     public function __construct($type, $productData) {
         $this->type          = $type;
         $this->id            = $productData['ID'];
+        $this->sharedID      = "{$this->type}-{$this->id}";
         $this->name          = $productData['Name'];
         $this->details       = $productData['Details'];
         $this->price         = $productData['Price'];
         $this->imageBasePath = $productData['ImageBasePath'];
-        $this->active        = true;//$productData['ActiveStatus'];
+        $this->active        = $productData['ActiveStatus'];
     }
 
 
@@ -49,33 +39,53 @@ class Product
      * @return string      the resolved image's path
      */
     public function image_source($imgID = 1) {
-        $src  = "res/products/$this->imageBasePath/$imgID";
+        $src  = "res/products/{$this->imageBasePath}/{$imgID}";
         $src .= file_exists("$src.jpg") ? ".jpg" : ".png";
         return $src;
     }
 
     /**
-     * Prints the current product's HTML representation
+     * Prints the current Product's HTML representation
+     * @return String  this Product's markup
      */
     public function write() {
         $img_src       = self::image_source(1);
+        $sharedID      = htmlspecialchars($this->sharedID);
+        $name          = htmlspecialchars($this->name    );
 
         $productMarkup = <<<MARKUP
-                            <li class="store-item">
-                              <div>
-                                <img alt="$this->name" class="tile" src="$img_src" />
-                              </div>
-                              <div class="hover_actions">
-                                  <div>Details</div>
-                                  <div>Add to Cart</div>
-                              </div>
-                              <div>
-                                <span class="item-name">$this->name</span>
-                                <span class="item-price">$this->price</span>
-                              </div>
-                            </li>
+<li class="store-item" data-product-id="{$sharedID}">
+  <div>
+    <img alt="{$name}" class="tile" src="{$img_src}" />
+  </div>
+  <div class="hover_actions">
+      <div>Details</div>
+      <div>Add to Cart</div>
+  </div>
+  <div>
+    <span class="item-name">{$name}</span>
+    <span class="item-price">{$this->price}</span>
+  </div>
+</li>
 MARKUP;
 
         echo $productMarkup;
+    }
+
+
+    /**
+     * Prepares a Product details array ready to be JSON-encoded
+     * @return Array  an Array of Product details
+     */
+    public function data_encode() {
+        $data = [
+                  'id'      => $this->id,
+                  'name'    => htmlspecialchars($this->name   ),
+                  'details' => htmlspecialchars($this->details),
+                  'price'   => $this->price,
+                  'img_src' => self::image_source(1)
+                ];
+
+        return $data;
     }
 }
