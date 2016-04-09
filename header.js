e18bbe0b01ed370171e0d5d2f40697ccccf6792a
occupytheweb <afinegameofnil@gmail.com>
2016-04-09 21:37:46 +0400

    Header script :: Extend tab activation methods

--- header.js
+++ header.js
@@ -8,6 +8,19 @@ header.components = {
     stickyMenu : {
                     navbar   : dom.id_get("sticky-menu"),
 
+                    tabs     : dom.classes_get("tab"),
+
+                    activate : function(tabName) {
+                        styles.remove_class(this.tabs, "active");
+                        var i = this.tabs.length;
+                        while(i--) {
+                            var thisTab = this.tabs[i];
+                            if ( dom.text_is(thisTab, tabName) ) {
+                                styles.add_class(thisTab, "active");
+                            }
+                        }
+                    },
+
                     y_offset : function() { return window.pageYOffset; },
 
                     freeze   : function() {
@@ -19,7 +32,7 @@ header.components = {
                     },
 
                     handler  : function() {
-                        if( this.y_offset() > 100) {
+                        if (this.y_offset() > 100) {
                             this.freeze();
                         } else {
                             this.release();
