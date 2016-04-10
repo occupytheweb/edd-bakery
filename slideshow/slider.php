32939285af02d7aa0a188b6e0615c7d453664989
yeshvin <yeshvin@live.com>
2016-04-10 15:25:38 +0400

    image slider
    please check the js code ive been trying to make it work but it isnt
    working, i just dont know why. i tried it with an online workspace and
    it is working overthere idk why it isnt with my localhost

---
+++ slideshow/slider.php
@@ -0,0 +1,65 @@
+<!Doctype html>
+<html>
+  <head>
+    <title>Edd</title>
+    <link rel="stylesheet" href="slider.css"/>
+    <script
+    src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.0.min.js">
+    </script>
+    <script>
+    $('#slider').lightSlider({
+        gallery: true,
+        item: 1,
+        loop:true,
+        slideMargin: 0,
+        thumbItem: 9
+    });
+    </script>
+  </head>
+  <body>
+    <div class="slideshow">
+      <ul id="slider">
+        <li data-thumb="cakes/c1.jpg">
+          <img src="cakes/c1.jpg"/>
+        </li>
+        <li data-thumb="cakes/c2.jpg">
+          <img src="cakes/c2.jpg"/>
+        </li>
+        <li data-thumb="cakes/c3.jpg">
+          <img src="cakes/c3.jpg"/>
+        </li>
+        <li data-thumb="cakes/c4.jpg">
+          <img src="cakes/c4.jpg"/>
+        </li>
+        <li data-thumb="cakes/c5.jpg">
+          <img src="cakes/c5.jpg"/>
+        </li>
+        <li data-thumb="cakes/c6.jpg">
+          <img src="cakes/c6.jpg"/>
+        </li>
+        <li data-thumb="cakes/c7.jpg">
+          <img src="cakes/c7.jpg"/>
+        </li>
+        <li data-thumb="cakes/c8.jpg">
+          <img src="cakes/c8.jpg"/>
+        </li>
+        <li data-thumb="cakes/c9.jpg">
+          <img src="cakes/c9.jpg"/>
+        </li>
+        <li data-thumb="cakes/c9.jpg">
+          <img src="cakes/c6.jpg"/>
+        </li>
+        <li data-thumb="cakes/c10.jpg">
+          <img src="cakes/c10.jpg"/>
+        </li>
+        <li data-thumb="cakes/c11.jpg">
+          <img src="cakes/c11.jpg"/>
+        </li>
+          <li data-thumb="cakes/c12.jpg">
+            <img src="cakes/c12.jpg"/>
+          </li>
+
+      </ul>
+    </div>
+
+</html>
