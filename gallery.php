cee4cb0692ca1a920625a121c007eb557a5084e3
Alan Jean <i_love_music_27@hotmail.com>
2016-04-10 12:57:16 +0400

    Merge from Nesh (#1)
    * About Us :: markup
    
    * Gallery :: bread section
    
    * Resources :: Gallery - cake images
    
    * Resources :: Gallery - pastries
    
    * Contact page
    
    * Gallery
    
    -cakes section
    -pastries section
    
    * Contact Us file: to add data in xml file
    
    * contact us modification according to the change in xml file
    
    * adding a tag feedback
    
    * xsd modification
    
    * about us
    
    * contact
    
    * gallery
    
    * image of the bakery : about us

---
+++ gallery.php
@@ -0,0 +1,91 @@
+<!DOCTYPE>
+<html>
+
+<head>
+<title>Gallery</title>
+  <link rel="stylesheet" href="css/display.css" />
+  <link rel="stylesheet" href="css/typography.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+  <link rel="stylesheet" href="css/grids.css" />
+  <link rel="stylesheet" href="css/glyphs.css" />
+  <link rel="stylesheet" href="css/dropdown.css" />
+  <link rel="stylesheet" href="css/store.css" />
+  <link rel="stylesheet" href="css/gallery.css" />
+</head>
+
+<body>
+
+  <?php
+    require_once('header.php');
+ ?> 
+ 
+<h2 class="g_h2">Gallery</h2>
+
+
+<div class="img">
+  <a target="_blank" href="breads.php">
+    <img src="res/breads/b6.jpg" alt="breads" width="300" height="200">
+  </a>
+	<div class="desc">Breads</div>
+</div>
+
+
+<div class="img">
+  <a target="_blank" href="cakes.php">
+    <img src="res/cakes/c8.jpg" alt="cakes" width="300" height="200">
+  </a>
+  <div class="desc">Cakes</div>
+</div>
+
+
+<div class="img">
+  <a target="_blank" href="pastries.php">
+    <img src="res/pastries/millefeuille.jpg" alt="pastries" width="300" height="200">
+  </a>
+  <div class="desc">Pastries</div>
+</div>
+
+
+ <footer>
+					<div class="col-sm-12">
+				  	<div class="col-md-offset-2 col-md-3">
+
+						</div>
+
+						<div class="col-md-offset-0 col-md-3">
+
+						</div>
+
+
+
+						<div class="col-md-offset-0 col-md-3">
+							<div class="vcard">
+								<h4 class="coral"><a href="contact/">Contact <span class="fn org">Epi de Dieu Ltd</span></a></h4>
+								<p class="tel">465 1001</p>
+
+								<div class="adr">
+									<p>
+										<span class="street-address">Rémi Ollier Street</span><br />
+										<span class="locality">Quatre-Bornes</span>, <span class="region">PW</span>, <span class="postal-code">0000</span>
+									</p>
+								</div>
+
+								<p><span class="tel"><span class="type">F<span class="d">ax</span></span>: <span class="value">(+230) 465 1001</span></span></p>
+								<p class="e">E<span class="d">mail</span>: <a href="&#99;&#111;&#110;&#116;&#97;&#99;&#116;&#64; &#108;&#111;&#99;&#97;&#108;&#104;&#111;&#115;&#116;" class="email that-thing"><span class="that-thing" style="unicode-bidi: bidi-override; direction: rtl;"> &#116;&#115;&#111;&#104;&#108;&#97;&#99;&#111;&#108;&#64;&#116;&#99;&#97;&#116;&#110;&#111;&#99; </span></a></p>
+							</div><!-- /.vcard -->
+						</div>
+					</div>
+					<div style="clear: both; height: 25px; width: 100%; bottom: 0px; background-image: url(res/ft.png);">
+					</div>
+				</footer>
+ 
+ 
+ 
+ 
+
+			   
+
+
+
+</body>
+</html>
