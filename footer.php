d6e72348e17944da828d823ed2727efd8d135304
yeshvin <yeshvin@live.com>
2016-04-13 21:13:06 +0400

    footer

---
+++ footer.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, links, icons" />
+
+
+	<link rel="stylesheet" href="css/footer.css">
+
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+</head>
+
+	<body>
+
+		<footer class="footer-page">
+
+			<div class="footer-right">
+
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-linkedin"></i></a>
+				<a href="#"><i class="fa fa-github"></i></a>
+
+			</div>
+
+			<div class="footer-left">
+        <p class="radio">Language:
+        <input type="radio" name="language" value="english" checked> English
+        <input type="radio" name="language" value="french"> French<br>
+        </p>
+				<p class="footer-links">
+					<a href="#">Home</a>
+					·
+					<a href="#">Store</a>
+					·
+					<a href="#">Bread</a>
+          ·
+          <a href="#">Store</a>
+					·
+					<a href="#">About</a>
+					·
+					<a href="#">Faq</a>
+					·
+					<a href="#">Contact</a>
+				</p>
+
+				<p class="compname">EdD- Epi de Dieu &copy; 2016</p>
+			</div>
+
+		</footer>
+
+	</body>
+
+</html>
